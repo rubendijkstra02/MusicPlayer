3685d82cd83c4da554739ac84506e930291ec544
rubendijkstra02 <ruupsel@gmail.com>
2017-11-02 10:04:53 +0100

    properties toegoevoegd
    sync ff

---
+++ MusicPlayer/BusinessLayer/Genre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    class Genre
+    {
+        private int genreID;
+
+        public int GenreID
+        {
+            get { return genreID; }
+            set { genreID = value; }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+}
